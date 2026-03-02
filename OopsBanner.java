49593653f2d99a008eeacb02bb148ece9598deea
Shayon Mazumder <sm2740@srmist.edu.in>
2026-03-02 21:18:58 +0530

    UC2: Print OOPS banner using ASCII art

--- OopsBanner.java
+++ OopsBanner.java
@@ -4,7 +4,7 @@ public class OopsBanner {
         System.out.println(" *****   *****  *****   ***** ");
         System.out.println("*     * *     * *    * *     *");
         System.out.println("*     * *     * *    * *      ");
-        System.out.println("*     * *     * *****   ****** ");
+        System.out.println("*     * *     * *****   ***** ");
         System.out.println("*     * *     * *            *");
         System.out.println("*     * *     * *            *");
         System.out.println(" *****   *****  *       ****** ");
